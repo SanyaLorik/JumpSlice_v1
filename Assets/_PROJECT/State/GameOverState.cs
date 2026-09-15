e091a253ac5efbcd130995738bcfee448b5f8b56
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-09-15 16:25:05 +0300

    Add game over

--- Assets/_PROJECT/State/GameOverState.cs
+++ Assets/_PROJECT/State/GameOverState.cs
@@ -8,13 +8,21 @@ public class GameOverState : StateBase
     [Header("Window")]
     [SerializeField] private WindowBase _gameOverWindow;
 
+    [Header("Animation")]
+    [SerializeField] private DOTweenAnimationGenericBase<CanvasGroup> _continueAnimtion;
+
     public override async UniTask Enter()
     {
+        _continueAnimtion.ResetToInitialState();
+
         await _gameOverWindow.Show();
+
+        _continueAnimtion.Animate();
     }
 
     public override UniTask Exit()
     {
+        _continueAnimtion.ResetToInitialState();
         throw new NotImplementedException();
     }
 }
